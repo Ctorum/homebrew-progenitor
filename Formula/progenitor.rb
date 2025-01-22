39be38bc2f243de00db8589c7e8751cc0a750911
VScope <dev.rezendevitor@gmail.com>
2025-01-22 15:13:36 -0300

    build(brew): new version
    BREAKING CHANGE:

--- Formula/progenitor.rb
+++ Formula/progenitor.rb
@@ -1,7 +1,7 @@
 class Progenitor < Formula
   desc "Project template generator CLI tool"
   homepage "https://github.com/Ctorum/progenitor"
-  version "0.2.4" 
+  version "0.2.5" 
   url "https://github.com/Ctorum/progenitor/releases/download/v0.2.5/progenitor-0.2.5.tar.gz"
   sha256 "8c6e60f6809201ad3f051bfa6b9a56c1434b4055b097bed91d20dcc9fd6d2ca3"
 
