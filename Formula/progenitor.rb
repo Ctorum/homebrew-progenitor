85269b08c64a8a05d33df9cb3e3591c546381304
VScope <dev.rezendevitor@gmail.com>
2025-01-22 15:02:50 -0300

    build(brew): new version

--- Formula/progenitor.rb
+++ Formula/progenitor.rb
@@ -4,11 +4,11 @@ class Progenitor < Formula
   version "0.2.1" 
 
   if OS.mac?
-    url "https://github.com/Ctorum/progenitor/releases/download/v0.2.0/progenitor-0.2.0.tar.gz"
-    sha256 "66349d4cb9cbf843decefe3a935462251a5c4d9033c8c3056a80310b449e96e0"
+    url "https://github.com/Ctorum/progenitor/releases/download/v0.2.4/progenitor-0.2.4.tar.gz"
+    sha256 "7abedc637b94ca59c08afae33b35bc794fae411d6b573468b44d5ad3d47cc10f"
   elsif OS.linux?
-    url "https://github.com/Ctorum/progenitor/releases/download/v0.2.0/progenitor-0.2.0.tar.gz"
-    sha256 "66349d4cb9cbf843decefe3a935462251a5c4d9033c8c3056a80310b449e96e0"
+    url "https://github.com/Ctorum/progenitor/releases/download/v0.2.4/progenitor-0.2.4.tar.gz"
+    sha256 "7abedc637b94ca59c08afae33b35bc794fae411d6b573468b44d5ad3d47cc10f"
   end
 
   def install
