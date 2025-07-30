3182d8d3fc5232b2c606db08688f38590f6ce52e
Ctorum <dev.rezendevitor@gmail.com>
2025-07-30 15:24:44 -0300

    feat: new pgen version
    added new commands(add, remove)
    
    BREAKING CHANGE: New commands

--- Formula/progenitor.rb
+++ Formula/progenitor.rb
@@ -1,9 +1,9 @@
 class Progenitor < Formula
   desc "Project template generator CLI tool"
   homepage "https://github.com/Ctorum/progenitor"
-  version "0.2.5" 
-  url "https://github.com/Ctorum/progenitor/releases/download/v0.2.5/progenitor-0.2.5.tar.gz"
-  sha256 "8c6e60f6809201ad3f051bfa6b9a56c1434b4055b097bed91d20dcc9fd6d2ca3"
+  version "0.3.0"
+  url "https://github.com/Ctorum/progenitor/releases/download/v0.3.0/progenitor.tar.gz"
+  sha256 "6e1562c87ba8e9e2e70651a2dff7637be6d47b5ffd2940fff2360902e286188c"
 
   def install
     bin.install "progenitor"
